9e740aad74a36c8a03fa4f191867989e400735ff
yifeihuangg <yifeih@uchicago.edu>
2017-09-29 20:17:24 -0500

    OSCV
    Comparison of Kernel regression with one-side cross-validation and traditional cross-validation

--- kde_2_band.py
+++ kde_2_band.py
@@ -8,11 +8,14 @@ Created on Wed Jul 19 22:38:10 2017
 
 import numpy as np
 from kernel_regression import *
+from matplotlib import cm
 import kernel_regression as kr
 import matplotlib.pyplot as plt
 import scipy.linalg as spl
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d import Axes3D
 from kdeOSCV import KernelRegression_OSCV
+
 
 def get_U(coord):
     """
@@ -75,8 +78,8 @@ def get_D(coord):
     D = np.zeros([len(coord),len(coord)])
     xsq = x*x
     ysq = y*y
-    D[0] = [xsq+5, 0]
-    D[1] = [0, 1]
+    D[0] = [0.5*np.sin(x+y)+1, 0]
+    D[1] = [0, xsq*xsq+ysq*ysq+1]
     return D
 
 
@@ -97,8 +100,8 @@ def get_dD(coord):
     x = coord[0]
     y = coord[1]
     dD = np.zeros([len(coord)])
-    dD[0] = 2*x
-    dD[1] = 0
+    dD[0] = 0.5*np.cos(x+y)
+    dD[1] = 4*y*y*y
     return dD
 
 def brownian_dynamics(nsteps,x0,force_method,get_divD,get_D,dt=0.001,kT=1.0):
@@ -166,44 +169,27 @@ def get_D_KDE_CV(trajs, pos, subsampling=1, dt=0.001):
     D : array
         Estimate of the diffusion constant corresponding to pos.
     """
+    print('start CV')
     kernel = kr.KernelRegression(kernel="rbf", gamma=np.logspace(-2, 2, 10))
     dim = np.size(trajs[0][0])
-    #if it is 1d dimension, then put all training datas in 1d array
-    if dim == 1 :
-        print('Dimension is 1')
-        y_trajs = np.zeros([len(trajs),len(trajs[0])-1])
-        x_trajs = np.zeros_like(y_trajs)
-        # Store all the y in one dataset and use all the x's and corresponding y's to make model
-        for n in xrange(len(trajs)):
-            for i in xrange(len(trajs[0])-1):
-                y_trajs[n][i] = ((trajs[n][i+1] - trajs[n][i])**2)
-                x_trajs[n][i] = trajs[n][i]
-        D = np.zeros(len(pos))
-        x = x_trajs.flatten()
-        y = y_trajs.flatten()
-        x.resize((len(x),1)) # Kernel.fit only takes in input matrix with shape(n,1), so change it into (n,1)
-        model = kernel.fit(x,y)
-        pos.resize((len(pos),1))# model.predict only take (x,1) size input
-        D = (model.predict(pos))/(dt*2*subsampling)
-    else:
-        print('Dimension is %d' %(dim))
-        D = np.zeros([dim,dim,len(pos)])
-        x_trajs = np.zeros([len(trajs), len(trajs[0])-1, np.size(trajs[0][0])])
-        for m in xrange(len(trajs)):
-            for n in xrange(len(trajs[0])-1):
-                x_trajs[m][n] = trajs[m][n]
-        
-        for i in xrange(dim):
-            for j in xrange(dim):
-                y_trajs = np.zeros([len(trajs), len(trajs[0])-1])
-                for m in xrange(len(trajs)):
-                    for n in xrange(len(trajs[0])-1):
-                        y_trajs[m][n] = (trajs[m][n+1][i]-trajs[m][n][i])*(trajs[m][n+1][j]-trajs[m][n][j])
+    print('Dimension is %d' %(dim))
+    D = np.zeros([dim,dim,len(pos)])
+    x_trajs = np.zeros([len(trajs), len(trajs[0])-1, np.size(trajs[0][0])])
+    for m in xrange(len(trajs)):
+        for n in xrange(len(trajs[0])-1):
+            x_trajs[m][n] = trajs[m][n]
 
-                x = x_trajs.reshape(len(trajs)*(len(trajs[0])-1),dim)
-                y = y_trajs.flatten()
-                model = kernel.fit(x,y)
-                D[i][j] = (model.predict(pos))/(dt*2*subsampling)
+    for i in xrange(dim):
+        for j in xrange(dim):
+            y_trajs = np.zeros([len(trajs), len(trajs[0])-1])
+            for m in xrange(len(trajs)):
+                for n in xrange(len(trajs[0])-1):
+                    y_trajs[m][n] = (trajs[m][n+1][i]-trajs[m][n][i])*(trajs[m][n+1][j]-trajs[m][n][j])
+
+            x = x_trajs.reshape(len(trajs)*(len(trajs[0])-1),dim)
+            y = y_trajs.flatten()
+            model = kernel.fit(x,y)
+            D[i][j] = (model.predict(pos))/(dt*2*subsampling)
 
     return D
 
@@ -228,44 +214,27 @@ def get_D_KDE_OSCV(trajs, pos, subsampling=1, dt=0.001):
     D : array
         Estimate of the diffusion constant corresponding to pos.
     """
+    print('start OSCV')
     kernel = KernelRegression_OSCV(kernel="rbf", gamma=np.logspace(-2, 2, 10))
     dim = np.size(trajs[0][0])
-    #if it is 1d dimension, then put all training datas in 1d array
-    if dim == 1 :
-        print('Dimension is 1')
-        y_trajs = np.zeros([len(trajs),len(trajs[0])-1])
-        x_trajs = np.zeros_like(y_trajs)
-        # Store all the y in one dataset and use all the x's and corresponding y's to make model
-        for n in xrange(len(trajs)):
-            for i in xrange(len(trajs[0])-1):
-                y_trajs[n][i] = ((trajs[n][i+1] - trajs[n][i])**2)
-                x_trajs[n][i] = trajs[n][i]
-        D = np.zeros(len(pos))
-        x = x_trajs.flatten()
-        y = y_trajs.flatten()
-        x.resize((len(x),1)) # Kernel.fit only takes in input matrix with shape(n,1), so change it into (n,1)
-        model = kernel.fit(x,y)
-        pos.resize((len(pos),1))# model.predict only take (x,1) size input
-        D = (model.predict(pos))/(dt*2*subsampling)
-    else:
-        print('Dimension is %d' %(dim))
-        D = np.zeros([dim,dim,len(pos)])
-        x_trajs = np.zeros([len(trajs), len(trajs[0])-1, np.size(trajs[0][0])])
-        for m in xrange(len(trajs)):
-            for n in xrange(len(trajs[0])-1):
-                x_trajs[m][n] = trajs[m][n]
-        
-        for i in xrange(dim):
-            for j in xrange(dim):
-                y_trajs = np.zeros([len(trajs), len(trajs[0])-1])
-                for m in xrange(len(trajs)):
-                    for n in xrange(len(trajs[0])-1):
-                        y_trajs[m][n] = (trajs[m][n+1][i]-trajs[m][n][i])*(trajs[m][n+1][j]-trajs[m][n][j])
+    print('Dimension is %d' %(dim))
+    D = np.zeros([dim,dim,len(pos)])
+    x_trajs = np.zeros([len(trajs), len(trajs[0])-1, np.size(trajs[0][0])])
+    for m in xrange(len(trajs)):
+        for n in xrange(len(trajs[0])-1):
+            x_trajs[m][n] = trajs[m][n]
 
-                x = x_trajs.reshape(len(trajs)*(len(trajs[0])-1),dim)
-                y = y_trajs.flatten()
-                model = kernel.fit(x,y)
-                D[i][j] = (model.predict(pos))/(dt*2*subsampling)
+    for i in xrange(dim):
+        for j in xrange(dim):
+            y_trajs = np.zeros([len(trajs), len(trajs[0])-1])
+            for m in xrange(len(trajs)):
+                for n in xrange(len(trajs[0])-1):
+                    y_trajs[m][n] = (trajs[m][n+1][i]-trajs[m][n][i])*(trajs[m][n+1][j]-trajs[m][n][j])
+
+            x = x_trajs.reshape(len(trajs)*(len(trajs[0])-1),dim)
+            y = y_trajs.flatten()
+            model = kernel.fit(x,y)
+            D[i][j] = (model.predict(pos))/(dt*2*subsampling)
 
     return D
 
@@ -275,7 +244,6 @@ def main():
     trajs = []
     initial = np.linspace(-2.,2.,16)
     kT = 1.0
-
     dt = 0.001
     nsteps = 20
 
@@ -294,92 +262,150 @@ def main():
         for n in xrange(len(hist_edges)-1):
             pos.append ([(hist_edges[m]+hist_edges[m+1])/2,(hist_edges[n]+hist_edges[n+1])/2])
     pos = np.array(pos)
-    
-    
+
+
     D_CV= get_D_KDE_CV(trajs,pos,subsampling,dt)
     D_OSCV=get_D_KDE_OSCV(trajs,pos,subsampling,dt)
     realD = np.array([get_D(pos_i) for pos_i in pos])
-    
-    print (np.shape(pos))
-    print(np.shape(D_CV))
-    
+
+
+
+
     #Plot estimation and true D values
-    plt.figure()
+    print "start plotting"
+# print D[1][1]
 #   scatter plot
-    vmax=np.amax(realD[:,0,0])+0.85*np.amax(realD[:,0,0])
-    vmin=np.amin(realD[:,0,0])-0.85*np.amax(realD[:,0,0])
-    plt.subplot(1,3,1)
+    vmax=np.amax(realD[:,0,0])+0.5*np.amax(realD[:,0,0])
+    vmin=np.amin(realD[:,0,0])-0.5*np.amax(realD[:,0,0])
+
+    plt.figure()
+    plt.subplot(131, aspect='equal', adjustable='box-forced')
+    ax = plt.gca()
     SC_1 = plt.scatter(pos[:,0], pos[:,1], c=D_CV[0,0,:],linewidth='0',vmax=vmax,vmin=vmin)
-    plt.xticks(size = 8)
-    plt.yticks(size = 8)
-    plt.tick_params(axis='both', which='major', labelsize=6)
-    plt.tick_params(axis='both', which='minor', labelsize=6)
-    plt.colorbar(SC_1)
-    plt.xlabel('x positions')
-    plt.ylabel('y position')
-    plt.title('KR CV')
-    
-    plt.subplot(1,3,2)
+    diver = make_axes_locatable(ax)
+    cax = diver.append_axes("right", size="5%", pad = 0.05)
+    plt.colorbar(SC_1,cax=cax)
+    # plt.xlabel('x positions')
+    # plt.ylabel('y position')
+    # plt.title('KR CV')
+
+    plt.subplot(132, aspect='equal', adjustable='box-forced')
+    ax = plt.gca()
     SC_2 = plt.scatter(pos[:,0], pos[:,1], c=D_OSCV[0,0,:],linewidth='0',vmax=vmax,vmin=vmin)
-    plt.xticks(size = 8)
-    plt.yticks(size = 8)
-    plt.tick_params(axis='both', which='major', labelsize=6)
-    plt.tick_params(axis='both', which='minor', labelsize=6)
-    plt.colorbar(SC_2)
-    plt.xlabel('x positions')
-    plt.ylabel('y position')
-    plt.title('KR OSCV')
-    
-    plt.subplot(1,3,3)
+    diver = make_axes_locatable(ax)
+    cax = diver.append_axes("right", size="5%", pad = 0.05)
+    plt.colorbar(SC_2,cax=cax)
+    # plt.xlabel('x positions')
+    # plt.ylabel('y position')
+    # plt.title('KR OSCV')
+
+    plt.subplot(133, aspect='equal', adjustable='box-forced')
+    ax = plt.gca()
     SC_3 = plt.scatter(pos[:,0],pos[:,1], c=realD[:,0,0], linewidth='0',vmax=vmax,vmin=vmin)
-    plt.xticks(size = 5)
-    plt.yticks(size = 5)
-    plt.tick_params(axis='both', which='major', labelsize=6)
-    plt.tick_params(axis='both', which='minor', labelsize=6)
-    plt.colorbar(SC_3)
-    plt.xlabel('x positions')
-    plt.ylabel('y position')
-    plt.title('RealD')
+    diver = make_axes_locatable(ax)
+    cax = diver.append_axes("right", size="5%", pad = 0.05)
+    plt.colorbar(SC_3,cax=cax)
+    # plt.xlabel('x positions')
+    # plt.ylabel('y position')
+    # plt.title('RealD')
+
     plt.legend()
+    plt.tight_layout()
     plt.show()
-#   plt.savefig('KDE_2D')
+    plt.close()
+
+    #   3D plt
+    X = pos[:,0]
+    Y = pos[:,1]
+    Z_CV = D_CV[0,0,:]
+    Z_OSCV = D_OSCV[0,0,:]
+    Zreal = realD[:,0,0]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(131, projection='3d',aspect='equal', adjustable='box-forced')
+    SP_1 = ax.plot_trisurf(X, Y, Z_CV, cmap=cm.coolwarm, linewidth = '0', vmax = vmax, vmin = vmin,antialiased=False)
+    fig.colorbar(SP_1,fraction=0.04,pad = 0.15)
+
+    ax = fig.add_subplot(132, projection='3d',aspect='equal', adjustable='box-forced')
+    SP_2 = ax.plot_trisurf(X, Y, Z_OSCV, cmap=cm.coolwarm, linewidth = '0', vmax = vmax, vmin = vmin,antialiased=False)
+    fig.colorbar(SP_2,fraction=0.04,pad = 0.15)
+
+    ax = fig.add_subplot(133, projection='3d',aspect='equal', adjustable='box-forced')
+    SP_3 = ax.plot_trisurf(X, Y, Zreal, cmap=cm.coolwarm, linewidth = '0', vmax = vmax, vmin = vmin,antialiased=False)
+    fig.colorbar(SP_3,fraction=0.04,pad = 0.15)
+
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+
+
+
+
+# print D[2][2]
+#   scatter plot
+    vmax=np.amax(realD[:,1,1])+0.5*np.amax(realD[:,1,1])
+    vmin=np.amin(realD[:,1,1])-0.5*np.amax(realD[:,1,1])
+
+    plt.subplot(131, aspect='equal', adjustable='box-forced')
+    ax = plt.gca()
+    SC_1 = plt.scatter(pos[:,0], pos[:,1], c=D_CV[1,1,:],linewidth='0',vmax=vmax,vmin=vmin)
+    diver = make_axes_locatable(ax)
+    cax = diver.append_axes("right", size="5%", pad = 0.05)
+    plt.colorbar(SC_1,cax=cax)
+    # plt.xlabel('x positions')
+    # plt.ylabel('y position')
+    # plt.title('KR CV')
+
+    plt.subplot(132, aspect='equal', adjustable='box-forced')
+    ax = plt.gca()
+    SC_2 = plt.scatter(pos[:,0], pos[:,1], c=D_OSCV[1,1,:],linewidth='0',vmax=vmax,vmin=vmin)
+    diver = make_axes_locatable(ax)
+    cax = diver.append_axes("right", size="5%", pad = 0.05)
+    plt.colorbar(SC_2,cax=cax)
+    # plt.xlabel('x positions')
+    # plt.ylabel('y position')
+    # plt.title('KR OSCV')
+
+    plt.subplot(133, aspect='equal', adjustable='box-forced')
+    ax = plt.gca()
+    SC_3 = plt.scatter(pos[:,0],pos[:,1], c=realD[:,1,1], linewidth='0',vmax=vmax,vmin=vmin)
+    diver = make_axes_locatable(ax)
+    cax = diver.append_axes("right", size="5%", pad = 0.05)
+    plt.colorbar(SC_3,cax=cax)
+    # plt.xlabel('x positions')
+    # plt.ylabel('y position')
+    # plt.title('RealD')
+
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
     plt.close()
 
 #   3D plt
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_xlabel('x position')
-    ax.set_ylabel('y position')
-    ax.set_zlabel('D')
-    plt.title('Kernel_CV vs. Real')
     X = pos[:,0]
     Y = pos[:,1]
-    Z = D_CV[0,0,:]
-    Zreal = realD[:,0,0]
-    ax.scatter(X, Y, Z,c = 'y',marker = 'o',label='kernel',linewidth = '0')
-    ax.scatter(X,Y,Zreal, c = 'b', marker = '^', label='real', linewidth = '0')
-    ax.set_zlim3d(vmin,vmax)
+    Z_CV = D_CV[1,1,:]
+    Z_OSCV = D_OSCV[1,1,:]
+    Zreal = realD[:,1,1]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(131, projection='3d',aspect='equal', adjustable='box-forced')
+    SP_1 = ax.plot_trisurf(X, Y, Z_CV, cmap=cm.coolwarm, linewidth = '0', vmax = vmax, vmin = vmin,antialiased=False)
+    plt.colorbar(SP_1,fraction=0.04,pad = 0.15)
+
+    ax = fig.add_subplot(132, projection='3d',aspect='equal', adjustable='box-forced')
+    SP_2 = ax.plot_trisurf(X, Y, Z_OSCV, cmap=cm.coolwarm, linewidth = '0', vmax = vmax, vmin = vmin,antialiased=False)
+    plt.colorbar(SP_2,fraction=0.04,pad = 0.15)
+
+    ax = fig.add_subplot(133, projection='3d',aspect='equal', adjustable='box-forced')
+    SP_3 = ax.plot_trisurf(X, Y, Zreal, cmap=cm.coolwarm, linewidth = '0', vmax = vmax, vmin = vmin,antialiased=False)
+    plt.colorbar(SP_3,fraction=0.04,pad = 0.15)
+
     ax.legend()
+    plt.tight_layout()
     plt.show()
-#   fig.savefig('3d_scatter')
-    plt.close()
-    
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_xlabel('x position')
-    ax.set_ylabel('y position')
-    ax.set_zlabel('D')
-    plt.title('Kernel_OSCV vs. Real')
-    X = pos[:,0]
-    Y = pos[:,1]
-    Z = D_OSCV[0,0,:]
-    Zreal = realD[:,0,0]
-    ax.scatter(X, Y, Z,c = 'y',marker = 'o',label='kernel',linewidth = '0')
-    ax.scatter(X,Y,Zreal, c = 'b', marker = '^', label='real', linewidth = '0')
-    ax.set_zlim3d(vmin,vmax)
-    ax.legend() 
-    plt.show()
-#   fig.savefig('3d_scatter')
     plt.close()
 
     return
